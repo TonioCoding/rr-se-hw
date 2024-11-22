2cd172c19210ed24d7b24fdcb7ebb775ab0ea436
TonioCoding <ToneBone7960@gmail.com>
2024-11-21 20:15:14 -0500

    added shadcn components to project

--- src/app/components/steps/Step1.tsx
+++ src/app/components/steps/Step1.tsx
@@ -1,5 +1,6 @@
 import { FormDataObj, OnNext } from "@/app/steps/[step]/page";
-
+import { Card, CardFooter, CardHeader, CardTitle } from "@/app/ui/Card";
+// account type
 export default function Step1({
   formData,
   onNext,
@@ -16,15 +17,22 @@ export default function Step1({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Account Type:
-        <select name="accountType" defaultValue={formData.accountType || ""}>
-          <option value="personal">Personal</option>
-          <option value="business">Business</option>
-        </select>
-      </label>
-      <button type="submit">Next</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Account Type:
+          <select name="accountType" defaultValue={formData.accountType || ""}>
+            <option value="personal">Personal</option>
+            <option value="business">Business</option>
+          </select>
+        </label>
+        <button type="submit">Next</button>
+      </form>
+      <Card className="bg-red-500">
+        <CardTitle>Hey</CardTitle>
+        <CardHeader>hey</CardHeader>
+        <CardFooter>bye</CardFooter>
+      </Card>
+    </>
   );
 }

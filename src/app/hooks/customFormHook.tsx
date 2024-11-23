b67bcbb140be255c87f8aa25593f05021d463e43
TonioCoding <ToneBone7960@gmail.com>
2024-11-22 21:46:14 -0500

    created custom form hook

--- src/app/hooks/customFormHook.tsx
+++ src/app/hooks/customFormHook.tsx
@@ -4,13 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 
-/* const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-}); */
-
-const personalInformationFormSchema = z.object({
+export const personalInformationFormSchema = z.object({
   firstName: z.string().min(8, {
     message: "",
   }),
@@ -23,22 +17,22 @@ const personalInformationFormSchema = z.object({
   dateOfBirth: z.string().min(8, {
     message: "",
   }),
-  avatar: z
+  /*  avatar: z
     .instanceof(File)
     .refine((file) => file.size <= 5 * 1024 * 1024, {
       message: "File size must not exceed 5MB",
     })
     .refine((file) => ["image/jpeg", "image/png"].includes(file.type), {
       message: "Only JPEG or PNG files are allowed",
-    }),
+    }), */
 });
 
-const contactInformationFormSchema = z.object({
-  emailAddress: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+export const contactInformationFormSchema = z.object({
+  emailAddress: z.string().min(8, {
+    message: "Username must be at least 8 characters.",
   }),
-  phoneNumber: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  phoneNumber: z.number().min(10, {
+    message: "Phone number must be at least 10 characters.",
   }),
   streetAddress: z.string().min(2, {
     message: "Username must be at least 2 characters.",
@@ -58,6 +52,16 @@ const contactInformationFormSchema = z.object({
 });
 
 export function usePersonalInformationForm() {
+  const form = useForm<z.infer<typeof personalInformationFormSchema>>({
+    resolver: zodResolver(personalInformationFormSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      middleName: "",
+      dateOfBirth: "",
+    },
+  });
+
   const {
     register,
     handleSubmit,
@@ -72,13 +76,14 @@ export function usePersonalInformationForm() {
     },
   });
 
-  const onSubmit: SubmitHandler<typeof personalInformationFormSchema> = (
-    data
-  ) => {
+  type FormSchemaType = z.infer<typeof personalInformationFormSchema>;
+
+  const onSubmit: SubmitHandler<FormSchemaType> = (data) => {
     console.log(data);
   };
 
   return {
+    ...form,
     register,
     handleSubmit,
     errors,
@@ -87,15 +92,11 @@ export function usePersonalInformationForm() {
 }
 
 export function useContactInformationForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<z.infer<typeof contactInformationFormSchema>>({
+  const form = useForm<z.infer<typeof contactInformationFormSchema>>({
     resolver: zodResolver(contactInformationFormSchema),
     defaultValues: {
       emailAddress: "",
-      phoneNumber: "",
+      phoneNumber: 0,
       streetAddress: "",
       unit: "",
       city: "",
@@ -104,13 +105,31 @@ export function useContactInformationForm() {
     },
   });
 
-  const onSubmit: SubmitHandler<typeof contactInformationFormSchema> = (
-    data
-  ) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof contactInformationFormSchema>>({
+    resolver: zodResolver(contactInformationFormSchema),
+    defaultValues: {
+      emailAddress: "",
+      phoneNumber: 0,
+      streetAddress: "",
+      unit: "",
+      city: "",
+      state: "",
+      postalCode: "",
+    },
+  });
+
+  type FormSchemaType = z.infer<typeof contactInformationFormSchema>;
+
+  const onSubmit: SubmitHandler<FormSchemaType> = (data) => {
     console.log(data);
   };
 
   return {
+    ...form,
     register,
     handleSubmit,
     errors,

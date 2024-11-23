d05f146b542d687b816fdd52641cc5e39007aa40
TonioCoding <ToneBone7960@gmail.com>
2024-11-23 16:27:55 -0500

    creating step 2 UI

--- src/app/components/steps/Step2.tsx
+++ src/app/components/steps/Step2.tsx
@@ -1,22 +1,20 @@
+"use client";
+
 import { OnBack, OnNext } from "@/app/steps/[step]/page";
 import { Button } from "@/app/ui/Button";
+import PersonalInformationForm from "../PersonalInformationForm";
 
-export default function Step4({
-  onNext,
-  onBack,
-}: {
-  onNext: OnNext;
-  onBack: OnBack;
-}) {
+export default function Step4({ onBack }: { onNext: OnNext; onBack: OnBack }) {
   return (
     <div>
-      hey
+      <h1></h1>
+      <PersonalInformationForm />
       <Button onClick={onBack} className="w-[100px] bg-blue-500">
         Back
       </Button>
-      <Button onClick={onNext} className="w-[100px] bg-blue-500">
+      {/* <Button onClick={onNext} className="w-[100px] bg-blue-500">
         Next
-      </Button>
+      </Button> */}
     </div>
   );
 }

f69e5a1c9fb791b11ba0eefc3d8370b106240060
TonioCoding <ToneBone7960@gmail.com>
2024-11-26 03:42:43 -0500

    styled home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,19 @@ import { Button } from "./ui/Button";
 export default function Home() {
   const router = useRouter();
   return (
-    <section className="text-black text-center mt-[1rem]">
-      <div>
-        <h1>RR SE HW KYC App</h1>
-        <p>
+    <section className="text-black text-center grid grid-cols-1 items-center">
+      <div className="grid grid-cols-1 items-center gap-[1rem]">
+        <h1 className="text-xl underline underline-offset-8">
+          RR SE HW &#8208; KYC App
+        </h1>
+        <p className="text-gray-500">
           Lightweight application to simulate Know Your Customer form process
         </p>
         <Button
           onClick={() => {
             router.push(`/steps/account-type`);
           }}
-          className="bg-blue-500 max-w-[100px]"
+          className="bg-blue-500 max-w-[100px] place-self-center"
         >
           Start
         </Button>

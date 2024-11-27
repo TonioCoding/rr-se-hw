be1d8957f0471c3ea05e82d1bbf854feecf7fac4
TonioCoding <ToneBone7960@gmail.com>
2024-11-27 01:20:31 -0500

    added github icon link in navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { IconContext } from "react-icons";
+import { FaGithub } from "react-icons/fa";
+
 export default function Navbar() {
   return (
-    <nav className="flex justify-start pl-[1rem] py-[0.5rem] text-white bg-zinc-400">
+    <nav className="flex justify-between px-[1rem] items-center py-[0.5rem] text-white bg-zinc-400">
       <h1>KYC Form</h1>
+      <IconContext.Provider value={{ size: "1.5rem" }}>
+        <a
+          title="github-link"
+          href="https://github.com/TonioCoding/rr-se-hw"
+          className="hover:text-gray-300 transition-all ease-in-out duration-300"
+        >
+          <FaGithub />
+        </a>
+      </IconContext.Provider>
     </nav>
   );
 }
